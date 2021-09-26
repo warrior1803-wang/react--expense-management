fcaaf57cade353d54b73884ac80f4295e5eba1f4
warrior1803-wang <60429051+warrior1803-wang@users.noreply.github.com>
2021-09-26 19:54:33 +0800

    change style of ExpenseDate

---
+++ .history/src/componets/ExpenseDate_20210926195300.js
@@ -0,0 +1,15 @@
+import "../style/ExpenseDate.css"
+function ExpenseDate(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+    return(
+        <div className="expense-date">
+        <div className>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+    )
+}
+
+export default ExpenseDate;
